a762144aebdbbfb201a603d6de8297cf5910b537
Yves Kleny <yvesk.martins@gmail.com>
2025-03-23 10:45:02 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O amor não é a única constante para um relacionamento</h1>
+    return <h1>O amor não é a única constante para um relacionamento, é apenas uma delas.</h1>
 }
 
 export default Home;
