fcf85719611f300880bb7c7178173cf75b27e3ee
Yves Kleny <yvesk.martins@gmail.com>
2025-03-31 12:39:38 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({
     message:
       "um autoelogio pode ser tão importante para autoestima quanto receber um elogio de outra pessoa",
