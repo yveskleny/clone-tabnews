effbc6587ac19b26eb2cb60e9ffdb71bdf7cb6e2
Yves Kleny <yvesk.martins@gmail.com>
2025-04-15 12:19:34 -0300

    bug fix at migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -14,24 +14,31 @@ export default async function migrations(request, response) {
     migrationsTable: "pgmigrations",
   };
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-  if (request.method == "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  try {
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
+    }
+    if (request.method == "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
 
-    await dbClient.end();
+      await dbClient.end();
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);
+    return response.status(405).end();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await dbClient.end();
   }
-  return response.status(405).end();
 }
