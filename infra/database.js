6f8804ed08c06129f0830288932468641eae0dd1
Yves Kleny <yvesk.martins@gmail.com>
2025-04-25 12:34:53 -0300

    fix `eslint` linting

--- infra/database.js
+++ infra/database.js
@@ -27,11 +27,11 @@ async function getNewClient() {
   await client.connect();
   return client;
 }
-
-export default {
+const database = {
   query,
   getNewClient,
 };
+export default database;
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
