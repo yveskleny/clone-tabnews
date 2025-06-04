d471895f199193a647adb7a12b2ddc4bde548e40
Yves Kleny <yvesk.martins@gmail.com>
2025-06-04 11:04:35 -0300

    feat: create and use `orchestrator.createUser()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,6 +1,9 @@
 import retry from "async-retry";
+import { faker } from "@faker-js/faker";
+
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
+import user from "models/user.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -25,14 +28,24 @@ async function clearDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
+async function createUser(userObject) {
+  return await user.create({
+    username:
+      userObject?.username || faker.internet.username().replace(/[_.-]/g, ""),
+    email: userObject?.email || faker.internet.email(),
+    password: userObject?.password || "validpassword",
+  });
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
+  createUser,
 };
-
-async function runPendingMigrations() {
-  await migrator.runPendingMigrations();
-}
 
 export default orchestrator;
